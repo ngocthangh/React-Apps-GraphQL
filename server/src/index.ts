40279bac6c61666344fe2b1ca0dbaed07d394cab
thanghn <thanghn@antsomi.com>
2021-12-05 16:39:09 +0700

    sync first table user

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,18 +2,20 @@ require('dotenv').config()
 import 'reflect-metadata'
 import express from 'express'
 import { createConnection } from 'typeorm'
+import { User } from './entities/User'
 
 
 const main = async() => {
     await createConnection({
         type: 'postgres',
-        database: 'reddit',
+        database: 'my-reddit',
         host: 'localhost',
         port: 54321,
         username: process.env.DB_USERNAME_DEV,
         password: process.env.DB_PASSWORD_DEV,
         logging: true,
-        synchronize: true
+        synchronize: true,
+        entities: [User]
     })
 
     const app = express()
