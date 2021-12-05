7193ae23aecde500b2b5bb988991e11745f1b7fa
thanghn <thanghn@antsomi.com>
2021-12-05 16:22:09 +0700

    config db done

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,24 @@
-console.log('HELLO WORLD 11133dfd3343')
+require('dotenv').config()
+import 'reflect-metadata'
+import express from 'express'
+import { createConnection } from 'typeorm'
+
+
+const main = async() => {
+    await createConnection({
+        type: 'postgres',
+        database: 'reddit',
+        host: 'localhost',
+        port: 54321,
+        username: process.env.DB_USERNAME_DEV,
+        password: process.env.DB_PASSWORD_DEV,
+        logging: true,
+        synchronize: true
+    })
+
+    const app = express()
+
+    app.listen(4000, () => console.log('SERVER STARTED PORT 4000'))
+}
+
+main().catch(error => console.log(error))
